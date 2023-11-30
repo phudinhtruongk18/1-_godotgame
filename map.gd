0f064176ea9cb3a000e4266b076ead3a6630bb52
provipgame <vipchivaythoi3@gmail.com>
2023-11-30 22:43:51 +0700

    thay doi loi choi game chien thuat co trang bi bom

--- map.gd
+++ map.gd
@@ -1,11 +1,23 @@
 extends Node2D
 
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
-
-
+	$player/Button.hide()
+	
+var count = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("somethingcanchange"):
+		count += 1
+		if count %2 == 0:
+			$player/Button.hide()
+		if count %2 != 0:
+			$player/Button.show()
 	pass
+
+
+
+func _on_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
--- player.gd
+++ player.gd
@@ -1,23 +1,40 @@
 extends CharacterBody2D
 
 
-@export var Bullet = load("res://bullet.tscn")
-@export var speed = 200
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
+@onready var sprite_2d = $Sprite2D
 
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func get_input():
-	if Input.is_action_just_pressed("shoot"):
-		shoot()
-		
-	look_at(get_global_mouse_position())
-	var input_direction = Input.get_vector("go_left", "go_right", "go_up", "go_down")
-	velocity = input_direction * speed
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	#animation
+	if (velocity.x > 1 || velocity.x <-1):
+		sprite_2d.animation = "running"
+	else: 
+		sprite_2d.animation = "default"
+	
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		sprite_2d.animation = "jumping"
+	
+	# Handle Jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
-func shoot():
-	var b = Bullet.instantiate()
-	add_child(b)
-	b.transform = $Node/Muzzle.transform
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 100)
+
+	move_and_slide()
+	
+	var isleft = velocity.x < 0
+	sprite_2d.flip_h = isleft
